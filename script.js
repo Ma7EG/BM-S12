c65cfe95a8651f3449c103ead55e47d827b1cf2a
Mahmoud Ali <73550267+Ma7EG@users.noreply.github.com>
2024-07-18 15:50:50 +0300

    Add All script ◘

---
+++ script.js
@@ -0,0 +1,163 @@
+
+let audio1 = document.getElementById('backgroundSound');
+
+function runScript() {
+    document.getElementById('myButton').style.display = 'none';
+    audio1.play();
+
+    const images = [
+        'assets/img1.png', 'assets/img2.png', 'assets/img3.png', 'assets/img4.png',
+        'assets/img5.png', 'assets/img6.png', 'assets/img7.png', 'assets/img8.png',
+        'assets/img1.png', 'assets/img2.png', 'assets/img3.png', 'assets/img4.png',
+        'assets/img5.png', 'assets/img6.png', 'assets/img7.png', 'assets/img8.png'
+    ];
+
+    
+
+    const gameBoard = document.getElementById('spongGame');
+    const resetButton = document.getElementById('reset-button');
+    let flipPhoto = [];
+    let equalTwo = 0;
+    const totalTwo = 8;
+    let timeout;
+
+    function shuffle(array) {
+        let currentIndex = array.length;
+        let tempVar, randomIndex;
+    
+        while (currentIndex !== 0) {
+            randomIndex = (Math.random() * currentIndex) | 0;
+            currentIndex--;
+    
+            tempVar = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = tempVar;
+        }
+    
+    
+    }
+
+
+    function createBoard() {
+     
+        shuffle(images);
+        images.forEach((src, index) => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.dataset.index = index;
+
+            const frontFace = document.createElement('img');
+            frontFace.src = 'assets/back.png';
+            frontFace.classList.add('back');
+
+            const backFace = document.createElement('img');
+            backFace.src = src;
+            backFace.classList.add('front');
+
+            card.appendChild(frontFace);
+            card.appendChild(backFace);
+            gameBoard.appendChild(card);
+
+
+            setTimeout(() => {
+                card.classList.add('flipped');
+                setTimeout(() => {
+                    card.classList.remove('flipped');
+                }, 5000);
+            }, 1000);
+
+
+            card.addEventListener('click', () => flipCard(card));
+        });
+
+        setTimeout(startGame, 6000);
+    }
+
+
+
+    function flipCard(card) {
+        if (flipPhoto.length === 2 || card.classList.contains('flipped')) return;
+
+        card.classList.add('flipped');
+        flipPhoto.push(card);
+
+        clearTimeout(timeout);
+
+        if (flipPhoto.length === 2) {
+           matchingPhoto();
+        } else {
+            timeout = setTimeout(() => {
+                card.classList.remove('flipped');
+                flipPhoto = [];
+            }, 5000);
+        }
+    }
+
+
+
+
+    function matchingPhoto() {
+        const [card1, card2] = flipPhoto;
+        const index1 = card1.dataset.index;
+        const index2 = card2.dataset.index;
+
+        if (images[index1] === images[index2]) {
+            setTimeout(() => {
+                card1.style.visibility = 'hidden';
+                card2.style.visibility = 'hidden';
+                flipPhoto = [];
+                equalTwo++;
+                if (equalTwo === totalTwo) {
+                }
+            }, 1000);
+
+
+        } else {
+            setTimeout(() => {
+                card1.classList.remove('flipped');
+                card2.classList.remove('flipped');
+                flipPhoto = [];
+            }, 1000);
+        }
+    }
+
+
+
+
+
+    function startGame() {
+        gameBoard.querySelectorAll('.card').forEach(card => card.classList.remove('flipped'));
+        flipPhoto = [];
+    }
+
+
+
+
+    resetButton.addEventListener('click', () => {
+        createBoard();
+        equalTwo = 0;
+        clearTimeout(timeout);
+    });
+
+    createBoard();
+}
+
+
+
+
+
+function mute() {
+   
+    var muteIcon = document.getElementById('muteIcon');
+    var muteText = document.getElementById('muteButton').querySelector('span');
+
+    if (audio1.muted) {
+        audio1.muted = false;
+        muteIcon.textContent = '🔈';
+        muteText.textContent = 'كتم الصوت';
+    } else {
+        audio1.muted = true;
+        muteIcon.textContent = '🔇';
+        muteText.textContent = 'إلغاء الكتم';
+    }
+}
